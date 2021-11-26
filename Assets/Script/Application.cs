efc844372133ee0cfcf31da8aa92a97095ac13ee
binhphancreator <binhpv.B18PT031@stu.ptit.edu.vn>
2021-11-26 21:17:42 +0700

    init unity 2019

---
+++ Assets/Script/Application.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Application
+{
+    private Dictionary<string, object> instances;
+
+    public Application()
+    {
+        instances = new Dictionary<string, object>();
+    }
+
+    public void singlethon(string key, object instance)
+    {
+        instances.Add(key, instance);
+    }
+
+    public T resolve<T>(string key)
+    {
+        if(!instances.ContainsKey(key)) throw new System.Exception("The instance is not exist");
+        return (T) instances[key];
+    }
+}
---
+++ Assets/Script/BaseComponent.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseComponent : MonoBehaviour
+{
+    public static Application application = new Application();
+
+    protected void bind()
+    {
+        application.singlethon(this.GetType().Name, this);
+    }
+}
