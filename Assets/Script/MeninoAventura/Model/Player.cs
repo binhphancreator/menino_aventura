4c0215c344b39abc6147d9514ea848d1894f7197
binhphancreator <binhpv.B18PT031@stu.ptit.edu.vn>
2021-12-12 10:32:27 +0700

    init mvc

---
+++ Assets/Script/MeninoAventura/Model/Player.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Core;
+
+namespace Model
+{
+    public class Player : BaseModel
+    {
+        public float jumpHeight = 4f;
+        public float gravity = -10f;
+        public float moveSpeed = 6f;
+    }
+}
