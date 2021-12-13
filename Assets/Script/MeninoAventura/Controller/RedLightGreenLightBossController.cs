ed774720e0d3697c65630ec2368941f1576552c7
luantran2k <75030293+luantran2k@users.noreply.github.com>
2021-12-13 15:27:39 +0700

    add sound controll and refactor code

---
+++ Assets/Script/MeninoAventura/Controller/RedLightGreenLightBossController.cs
@@ -0,0 +1,27 @@
+﻿using Model;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Controller
+{
+    public class RedLightGreenLightBossController : MonoBehaviour
+    {
+        public GameObject headBoss;
+        public AudioClip audioClip;
+        AudioSource song;
+        private RedLightGreenLightBossModel bossModel;
+
+        private void Start()
+        {
+            song = gameObject.AddComponent<AudioSource>();
+            song.clip = audioClip;
+            bossModel = new RedLightGreenLightBossModel(headBoss, song);
+        }
+
+        void Update()
+        {
+            bossModel.HeadRotate();
+        }
+    }
+}
---
+++ Assets/Script/MeninoAventura/Model/RedLightGreenLightBossModel.cs
@@ -0,0 +1,55 @@
+﻿using Core;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Model
+{
+    public class RedLightGreenLightBossModel
+    {
+        public GameObject headBoss;
+        public AudioSource song;
+        public float rotateSpeed;
+        private int rotateDirection;
+        public int minTimeSong, maxTimeSong, minTimeLook, maxTimeLook;
+        private float timeWait;
+
+        public RedLightGreenLightBossModel(GameObject gameObject, AudioSource audioSource)
+        {
+            this.headBoss = gameObject;
+            this.song = audioSource;
+            this.rotateSpeed = 150f;
+            this.rotateDirection = 1;
+            this.minTimeSong = 2;
+            this.maxTimeSong = 8;
+            this.minTimeLook = 2;
+            this.maxTimeLook = 4;
+            this.timeWait = 5;
+            song.Play();
+        }
+
+        public void HeadRotate()
+        {
+            timeWait -= Time.deltaTime;
+            if (timeWait < 0)
+            {
+                headBoss.transform.Rotate(Vector3.up, rotateDirection * rotateSpeed * Time.deltaTime);
+            }
+            float rotation = headBoss.transform.eulerAngles.y; //Get Head Rotation
+            if (rotation > 180 && rotateDirection == 1)
+            {
+                rotateDirection = -1;
+                timeWait = Random.Range(minTimeLook, maxTimeLook);
+                Debug.Log("Time look: " + timeWait);
+                song.Pause();
+            }
+            else if (rotation < 2 && rotateDirection == -1)
+            {
+                rotateDirection = 1;
+                timeWait = Random.Range(minTimeSong, maxTimeSong);
+                Debug.Log("Time sing: " + timeWait);
+                song.Play();
+            }
+        }
+    }
+}
