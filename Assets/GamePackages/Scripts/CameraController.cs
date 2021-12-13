2404bc587dff9b4aa063ac96187e45f354004c30
Traan Hai Nam <teenhainam2603@gmail.com>
2021-12-14 01:33:34 +0700

    add scene

---
+++ Assets/GamePackages/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private float mouseSensivity;
+    private Transform parent;
+
+
+
+    void Start()
+    {
+        parent = transform.parent;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rotate();
+    }
+
+    private void Rotate(){
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensivity * Time.deltaTime;
+        parent.Rotate(Vector3.up, mouseX);
+    }
+}
