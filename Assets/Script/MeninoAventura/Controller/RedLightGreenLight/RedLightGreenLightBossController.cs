97f9a8c114b393f43064e1a4a4becc059f16a211
luantran2k <75030293+luantran2k@users.noreply.github.com>
2021-12-14 16:09:52 +0700

    red light green light basic logic

---
+++ Assets/Script/MeninoAventura/Controller/RedLightGreenLight/RedLightGreenLightBossController.cs
@@ -0,0 +1,36 @@
+﻿using Core;
+using Model;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Controller
+{
+    public class RedLightGreenLightBossController : BaseController
+    {
+        public GameObject headBoss;
+        public AudioClip audioClip;
+        AudioSource song;
+        private RedLightGreenLightBossModel bossModel;
+
+        private void Start()
+        {
+            song = gameObject.AddComponent<AudioSource>();
+            song.clip = audioClip;
+            bossModel = new RedLightGreenLightBossModel(headBoss, song);
+        }
+
+        void Update()
+        {
+            bossModel.HeadRotate();
+            if (bossModel.getIsSing())
+            {
+                FirstLevelController.allowRun = true;
+            }
+            else
+            {
+                FirstLevelController.allowRun = false;
+            }
+        }
+    }
+}
