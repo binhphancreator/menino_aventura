dad20c88c93a415d2e239822325b85a65ec93086
Traan Hai Nam <teenhainam2603@gmail.com>
2021-12-16 15:16:46 +0700

    add scene2

---
+++ Assets/GamePackages/Scripts/StartScene/SrartControll.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SrartControll : MonoBehaviour
+{
+    UIManager ui;
+
+    private void Start()
+    {
+        ui = FindObjectOfType<UIManager>();
+    }
+    public void Back(){
+        ui.ShowMinigame2(false);
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    public void LoadMinigame2(){
+        SceneManager.LoadScene("Scene2");
+    }
+}
