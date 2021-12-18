b6e2d4c2d3743eaceb8b1e0813250a28649ef509
binhphancreator <binhpv.B18PT031@stu.ptit.edu.vn>
2021-12-18 17:52:19 +0700

    refactor start scene to mvc

---
+++ Assets/Scripts/MeninoAventura/Door.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Door : MonoBehaviour
+{
+    public GameObject menu;
+    public string minigame;
+
+    private void Start()
+    {
+        
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Cursor.lockState = CursorLockMode.None;
+			menu.SetActive(true);
+		}
+	}
+
+    void OnCollisionExit(Collision other) {
+        Cursor.lockState = CursorLockMode.Locked;
+        menu.SetActive(false);
+    }
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(minigame);
+    }
+}
