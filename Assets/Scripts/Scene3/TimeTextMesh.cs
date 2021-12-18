ef50b3d05c5559de41f0d605b43ab617d217f597
Traan Hai Nam <teenhainam2603@gmail.com>
2021-12-19 01:26:01 +0700

    change model

---
+++ Assets/Scripts/Scene3/TimeTextMesh.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeTextMesh : MonoBehaviour
+{
+     public float timeRemaining = 10;
+    public bool timerIsRunning = false;
+    public TextMeshPro timeText;
+    UI3Manager ui3;
+    bool isGameover;
+    Game3Controller gc;
+
+    private void Start()
+    {
+        // Starts the timer automatically
+        timerIsRunning = true;
+        ui3 = FindObjectOfType<UI3Manager>();
+        gc = FindObjectOfType<Game3Controller>();
+    }
+
+    void Update()
+    {
+        if(gc.IsGameOver()) return;
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                DisplayTime(timeRemaining);
+            }
+            else
+            {
+                Debug.Log("Time has run out!");
+                Cursor.lockState = CursorLockMode.None;
+                ui3.ShowGameOverPanel(true);
+                gc.SetGameOverState(true);
+                timeRemaining = 0;
+                timerIsRunning = false;
+            }
+        }
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        timeToDisplay += 1;
+
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60); 
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+        timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
